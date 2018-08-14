d41d01e298b693191fea98ff859c1f574f27ff59
anas.najim <anas.najim@gmail.com>
2018-08-14 12:46:01 +0300

    updte dialoug3

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
             else
             {
-                await context.PostAsync($"{this.count++}: You said this before{message.Text}");
+                await context.PostAsync($"{this.count++}: You said --{message.Text}");
                 context.Wait(MessageReceivedAsync);
             }
         }
